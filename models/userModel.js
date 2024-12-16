d73f6732296cdfa59414c6afc2b5447ca80eade5
suryoday <suryodaykaushik@gmail.com>
2024-12-16 14:34:27 +0530

    add user model

---
+++ models/userModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "username is required"],
+        lowercase: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "password is required"],
+        minlength: 6
+    },
+
+}, {
+    timestamps: true
+});
+
+
+//pre hook for bcrypt
+userSchema.pre('save', async function () {
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+//instance method to compare password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    const isMatch = bcrypt.compare(candidatePassword, this.password)
+    return isMatch
+}
+
+
+const User = mongoose.model("User", userSchema)
+export default User
