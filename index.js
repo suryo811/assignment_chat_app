49d8d1caf72fc7e39d090e9838336e41c0a0db44
suryoday <suryodaykaushik@gmail.com>
2024-12-16 22:46:36 +0530

    add socket io server

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config();
+import { createServer } from "http";
+import { Server } from "socket.io";
 import connectDb from './db.js';
 import globalErrorHandler from './middleware/errorHandler.js';
 import AppError from './utils/appError.js';
 import authRoute from './routes/authRoute.js'
+import socketController from './controller/socketController.js';
 
 const app = express()
 
@@ -17,23 +20,33 @@ app.use("/api/v1/auth", authRoute);
 
 
 
-app.all("*", (req, _) => {
-    throw new AppError(`Can't find ${req.originalUrl} on this server`, 404);
+app.all("*", (req, _, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 app.use(globalErrorHandler);
 
 
-const port = process.env.PORT || 3001
-const start = async () => {
+const PORT = process.env.PORT || 3001
+
+// Create HTTP server and initialize Socket.io
+const server = createServer(app);
+const io = new Server(server);
+
+// Pass the Socket.io instance to the controller
+socketController(io);
+
+
+// Start the server
+const startServer = async () => {
     try {
         await connectDb();
-        app.listen(port, () => {
-            console.log(`server running on port ${port}`);
-        })
+        server.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
     } catch (error) {
-        console.log(error);
+        console.error("Error starting the server:", error.message);
         process.exit(1); //unsuccessful exit
     }
-}
+};
 
-start()
+startServer();
