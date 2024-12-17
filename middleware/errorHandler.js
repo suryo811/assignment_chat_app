8430e6d56efc7991783119b207366240f9f07f73
suryoday <suryodaykaushik@gmail.com>
2024-12-17 13:00:30 +0530

    handle mongoose validation error

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -6,6 +6,11 @@ const globalErrorHandler = (err, req, res, next) => {
         msg: err.message || 'Something went very wrong. Try again later.'
     }
 
+    if (err.name === 'ValidationError') {
+        customError.msg = Object.values(err.errors).map((item) => item.message).join(', ')
+        customError.statusCode = 400
+    }
+
     //return res.status(500).json(err)
     return res.status(customError.statusCode).json({ msg: customError.msg })
 }
